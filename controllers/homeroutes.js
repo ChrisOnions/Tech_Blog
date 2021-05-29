5270b6a257d6a8b79442f3fc509767c9739b015c
Chris Onions <c.onions101@gmail.com>
2021-05-29 21:11:46 +1000

    Added  controllers/api/comments.js

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,14 +1,50 @@
 const router = require('express').Router();
 const { Project, User } = require('../models');
+const withAuth = require('../utils/auth');
 
-// get and render
 router.get('/', async (req, res) => {
-  res.render('index.html')
+  try {
+
+    // Pass serialized data and session flag into template
+    res.render('home');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}); module.exports = router;
+
+router.get('/profile', async (req, res) => {
+
+  if (req.session.logged_in) {
+    res.redirect('/profile')
+    return
+  }
+  res.render('home')
 })
 
-// get by id and render
 
-// Get user 
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
 
+  res.render('login');
+});
 
+router.get('/dashboard', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
 
+  res.render('dashboard');
+});
+
+router.get('/posts', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('post');
+});
